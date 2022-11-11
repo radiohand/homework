a7bd0eb87c9b0d3f862ac859f02c6fedba222691
Aleksandr Andreev <alexalexov61@gmail.com>
2022-11-11 23:23:44 +0300

    добавил описание

--- src/additional/strings/StringsMain2.java
+++ src/additional/strings/StringsMain2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class StringsMain2 {
     public static void main(String[] args) {
+        System.out.println("Конвертация чисел с плавающей запятой в прописной формат.");
         System.out.println("Введите число от - 999.999.999,99 до 999.999.999,99");
         System.out.println(StringsConverter.toString(getUserDouble()));
     }
