ddd1f1fbcb0d3b45bd3536d0468657488705675e
Aleksandr Andreev <alexalexov61@gmail.com>
2023-01-10 01:06:11 +0300

    Доработан TextFileUtilMain

--- src/home_work_6/runners/TextFileUtilMain.java
+++ src/home_work_6/runners/TextFileUtilMain.java
@@ -7,13 +7,20 @@ import java.util.*;
 
 public class TextFileUtilMain {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         Set<String> set = new HashSet<>();
         File textFile = new File("src/home_work_6/source/war_and_peace.txt");
-        System.out.println("Количество уникальных слов:");
-        System.out.println(TextFileUtils.fileToSet(set, textFile));
+        System.out.println("Количество уникальных слов:" + TextFileUtils.fileToSet(set, textFile));
 
-        List<Map.Entry<String, Integer>> list = TextFileUtils.findTopOfWords(textFile, 10);
-        System.out.println("Топ слов по использованию:");
-        System.out.println(list);
+        System.out.println("Введите размер выборки самых используемых слов:");
+        int topSize = scanner.nextInt();
+
+        List<Map.Entry<String, Integer>> list = TextFileUtils.findTopOfWords(textFile, topSize);
+        System.out.println("Топ " + list.size() + " слов по использованию:");
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println("Слово '" + entry.getKey() + "' используется в тексте " + entry.getValue() + " раз.");
+        }
     }
 }
